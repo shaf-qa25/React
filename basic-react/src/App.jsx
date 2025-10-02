d2a0e5727c7d9c46bddd9c6dde5c8230fdee8ea7
Shafqa Fatma <fatmashafqa4@gmail.com>
2025-10-02 11:23:52 +0530

    created a netflix card

---
+++ basic-react/src/App.jsx
@@ -0,0 +1,12 @@
+ const App=()=>{
+  return(
+    <>
+    <img src="qot.png" alt="qot.png" width="40%" height="40%"/>
+    <h2>Name:"Queen of Tears</h2>
+    <h3>Rating:8.3</h3>
+    <p>Summary:Queen of Tears is a K-drama about a chaebol heiress, Hong Hae-in, and her lawyer husband, Baek Hyun-woo, whose marriage is on the rocks. After discovering Hae-in has a brain tumor and only months to live, Hyun-woo initially sees it as a chance to avoid a difficult divorce, but their journey ultimately leads to a miraculous rekindling of their love and a fight against family rivals who are scheming to take over her company, the Queens Group.  
+</p>
+    </>
+  )
+};
+export default App
